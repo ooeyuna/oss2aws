747fce48cd7a893b5711ce2762743a896e136a44
siyu <chensiyi@agora.io>
2021-06-10 20:06:21 +0800

    add created_at fill task

--- slice_task_with_created_at.rb
+++ slice_task_with_created_at.rb
@@ -6,7 +6,7 @@ data = JSON.load(file)
 
 p data.length
 
-data.each_slice(3000).with_index do | slice, index |
-  File.open("tasks/#{index + 21}.json", 'w') { |file| file.write(JSON.dump(slice.map{|data| data["uuid"]})) }
+data.each_slice(10000).with_index do | slice, index |
+  File.open("fill_tasks/#{index + 21}.json", 'w') { |file| file.write(JSON.dump(slice.map{|data| data["uuid"]})) }
   
 end 
